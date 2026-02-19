5a8f4f8debc381c72f77831b853e8740084d72d6
daidarzzz <penealcachofa882@gmail.com>
2026-02-19 20:49:41 +0100

    aaa

--- segundo-trimestre/JS/main.js
+++ segundo-trimestre/JS/main.js
@@ -108,6 +108,164 @@ function ejercicio38_3() {
 
 function ejercicio38_4() {
 
-    
-    
+    let num = Number(prompt("Introduce un número: "))
+    for (let i = 1; i <= 10; i++) {
+        
+        alert("5 x " + i + " = " + (5*i))
+        
+    }
+
+}
+
+function ejercicio39() {
+
+    let x = "5"
+    let y = 5
+
+    alert("[x == y]: " + x == y)
+    alert("[x === y]: " + x === y)
+    alert(x = y)
+    alert("Nuevo x: " + x)
+
+}
+function ejercicio41() {
+
+    ejercicio41_Hola()
+    alert(mensaje)
+
+}
+
+function ejercicio41_Hola() {
+    let mensaje = "Hola"
+    alert(mensaje)
+}
+
+function ejercicio42() {
+
+    let num = Number(prompt("Introduce un número: "))
+    alert("El doble de " + num + " es " + doble(num))
+
+}
+
+function doble(n) {
+    return n * 2
+}
+
+
+function ejercicio43() {
+
+    let num = Number(prompt("Introduce un número: "))
+    alert(esPrimo(num))
+
+}
+
+function esPrimo(n) {
+
+    if (n <= 1) return false
+    for (let i = 2 ; i < n ; i++) {
+        if (n % i === 0) return false       
+    }
+    return true
+ 
+}
+
+function ejercicio46() {
+
+    let alumnos = [
+        {nombre: "Jacobo", nota: 1},
+        {nombre: "Joaquín", nota: 5},
+        {nombre: "Manuel", nota: 9}
+    ]
+
+    alert("Primer alumno: " + alumnos[0].nombre)
+
+    alumnos.forEach(alumno => {
+        alert(alumno.nombre + " tiene nota " + alumno.nota)
+    }
+
+    )
+
+}
+
+let nums = [10, 20, 30]
+function ejercicio49() {
+
+
+
+    nums.forEach( num => {
+        alert(num)
+    }
+
+    )
+
+
+}
+
+function ejercicio50() {
+
+    alert(nums.length)
+
+}
+
+function ejercicio52() {
+
+    let frutas = ["manzana", "banana", "naranja"]
+    let primeraFruta = frutas.shift()
+    alert(frutas)
+
+    let frutas2 = ["banana", "naranja"]
+    frutas2.unshift("manzana", "uva")
+    alert(frutas2)
+
+}
+
+function ejercicio53() {
+
+    let frutas = ["manzana", "pera"]
+    frutas.push("plátano")
+
+    frutas.forEach( fruta => {
+        alert(fruta)
+    }
+
+    )
+
+
+}
+function ejercicio54_1() {
+
+    let lenguajes = ["HTML", "CSS", "JavaScript"]
+    lenguajes.forEach(lenguaje => {
+        alert(lenguaje)
+    })
+
+
+}
+
+function ejercicio54_2() {
+
+    let coche = {
+        marca: "Ford",
+        modelo: "Fiesta",
+        año: "2018"
+    }
+    alert(coche.marca)
+
+}
+
+function ejercicio55_1() {
+    let alumnos = ["Ana", "Luis", "Pedro"]
+    alumnos.forEach(nombre => {
+        alert(nombre)
+    })
+}
+
+function ejercicio55_2() {
+
+    let productos = []
+    productos.push("camisa")
+    productos.push("zapatos")
+    productos.pop()
+    alert(productos)
+
 }
