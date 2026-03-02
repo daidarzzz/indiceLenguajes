10801b1ada58d88fd1af015d535eebe8cc77b176
Daidarz <davidandreica127@gmail.com>
2026-03-02 16:25:34 +0100

    no puc mes

--- segundo-trimestre/JS/miniTwitter/scriptTwitter.js
+++ segundo-trimestre/JS/miniTwitter/scriptTwitter.js
@@ -1,13 +1,26 @@
+let listaTweets = [] 
 
 function mostrarPost() {
-    const tweet = document.getElementById('tweets')
+    let tweet = document.getElementById('tweets')
     console.log(tweet.value)
 
     let parrafo = document.createElement("p")
+    parrafo.classList.add("tweet")
     parrafo.textContent = tweet.value
     parrafo.style.color = "white"
     parrafo.style.fontWeight = "bold"
     parrafo.style.fontSize = "2em"
     document.body.appendChild(parrafo)
+    listaTweets.push(tweet)
+}
 
+function crearPost() {
+    let tweet = document.getElementById('tweets')
+    listaTweets.unshift(tweet)
+}
+
+function eliminarPost() {
+    listaTweets.forEach(element => {
+        alert(element.value)
+    });
 }
